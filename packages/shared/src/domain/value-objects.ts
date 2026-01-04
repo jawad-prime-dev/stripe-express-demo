9cf5af1e8583c9ded7b25399ce97affce7c0173e
jawad-prime-dev <jawadahmed.dev@gmail.com>
2026-01-04 22:42:32 +0500

    More value objects

--- packages/shared/src/domain/value-objects.ts
+++ packages/shared/src/domain/value-objects.ts
@@ -1,14 +1,20 @@
 // ===== Branded Primitive Types =====
-export type CustomerId = string & { readonly __brand: "CustomerId" };
 export type Currency = string & { readonly __brand: "Currency" };
 export type Amount = number & { readonly __brand: "Amount" };
+export type IdempotencyKey = string & { readonly __brand: "IdempotencyKey" };
 export type Event = Buffer & { readonly __brand: "Event" };
 export type RawEvent = Buffer & { readonly __brand: "RawEvent" };
 export type RawEventVerificationSignature = string & { readonly __brand: "RawEventVerificationSignature" };
+export type PaymentConfirmationSecret = string & { readonly __brand: "PaymentConfirmationSecret" };
+export type CreatedAt = Date & { readonly __brand: "CreatedAt" };
+export type ModifiedAt = Date & { readonly __brand: "ModifiedAt" };
+export type UserId = string & { readonly __brand: "UserId" };
 
 
 // ===== Payment ID =====
-export abstract class PaymentId {}
+export abstract class PaymentId {
+    private readonly _brand!: void;
+}
 export class HasPaymentId extends PaymentId {
   constructor(public readonly value: string) {
     super();
@@ -18,10 +24,24 @@ export class NoPaymentId extends PaymentId {}
 
 
 // ===== Payment Reference =====
-export abstract class PaymentReference {}
+export abstract class PaymentReference {
+  protected readonly _brand!: void;
+}
 export class HasPaymentReference extends PaymentReference {
   constructor(public readonly value: string) {
     super();
   }
 }
 export class NoPaymentReference extends PaymentReference {}
+
+
+// ===== Customer ID =====
+export abstract class CustomerId {
+    private readonly _brand!: void;
+}
+export class HasCustomerId extends CustomerId {
+  constructor(public readonly value: string) {
+    super();
+  }
+}
+export class NoCustomerId extends CustomerId {}
